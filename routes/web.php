65ea0835d331ebe89e2cb74adf1cbeb43397fded
Md. Aminul Islam Fahim <cipfahim1999@gmail.com>
2023-08-03 15:35:00 +0600

    wlecome

--- routes/web.php
+++ routes/web.php
@@ -14,31 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    // Assume the file path is relative to the Laravel project's root
-    $filePath = public_path('data/sample.txt');
-
-    // Open the file for reading
-    $fileHandle = fopen($filePath, 'r');
-
-    if ($fileHandle) {
-        // Loop through the file line by line until the end of the file
-        while (($line = fgets($fileHandle)) !== false) {
-            // Process the current line here
-            // For example, you can echo the line or store it in an array, etc.
-            \App\Models\Subscriber::updateOrCreate([
-                'email' => trim($line)
-            ],[
-                'userID' => 1,
-                'list' => 1,
-                'timestamp' => time()
-            ]);
-            echo $line;
-        }
-
-        // Close the file handle when done reading
-        fclose($fileHandle);
-    } else {
-        // Handle the case when the file couldn't be opened
-        echo "Error opening the file.";
-    }
+   return view('welcome');
 });
